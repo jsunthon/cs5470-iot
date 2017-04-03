4231d69711f938e6159161714547001d73859d7c
James <jsunthon@gmail.com>
2017-04-02 20:31:49 -0700

    Fix issue where random() method of each enum kept returning the same value

--- src/main/java/models/TimeToLive.java
+++ src/main/java/models/TimeToLive.java
@@ -4,7 +4,7 @@ public enum TimeToLive {
     LOW, MEDIUM, HIGH;
 
     public static TimeToLive randomTimeToLive() {
-        int randInd = (int) Math.random() * TimeToLive.values().length;
+        int randInd = (int) (Math.random() * TimeToLive.values().length);
         return TimeToLive.values() [randInd];
     }
 }
