982ddebcd8bbeb337f620eaadc16e3d916c51780
shay <shay.nguyen@gmail.com>
2017-04-19 02:04:55 -0700

    Add discovery nested tests

--- src/test/java/models/DiscoverTests.java
+++ src/test/java/models/DiscoverTests.java
@@ -83,16 +83,16 @@ public class DiscoverTests {
 
         // Friend 1 and his 1 friend
         SocialNode friend1 = getFeatureNode(1, Feature.SMART_LIGHTING, Feature.SMART_PARKING);
-        friend1.addRelationship(getFeatureNode(100, Feature.randomFeature()), Relationship.FAMILY);
+        friend1.addRelationship(getFeatureNode(10, Feature.randomFeature()), Relationship.FAMILY);
 
         // Friend 2 and his 2 friends
         SocialNode friend2 = getFeatureNode(2, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend2.addRelationship(getFeatureNode(200, Feature.randomFeature()), Relationship.FAMILY);
-        friend2.addRelationship(getFeatureNode(201, Feature.randomFeature()), Relationship.FAMILY);
+        friend2.addRelationship(getFeatureNode(20, Feature.randomFeature()), Relationship.FAMILY);
+        friend2.addRelationship(getFeatureNode(21, Feature.randomFeature()), Relationship.FAMILY);
 
         // Friend 3 and his 1 friend
         SocialNode friend3 = getFeatureNode(3, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend3.addRelationship(getFeatureNode(300, Feature.randomFeature()), Relationship.FAMILY);
+        friend3.addRelationship(getFeatureNode(30, Feature.randomFeature()), Relationship.FAMILY);
 
         // Add friends to main
         main.addRelationship(friend1, Relationship.FAMILY.FRIEND);
@@ -117,17 +117,17 @@ public class DiscoverTests {
 
         // Friend 1 and his 1 friend
         SocialNode friend1 = getFeatureNode(1, Feature.SMART_LIGHTING, Feature.SMART_PARKING);
-        friend1.addRelationship(getFeatureNode(100, Feature.randomFeature()), Relationship.FAMILY);
+        friend1.addRelationship(getFeatureNode(10, Feature.randomFeature()), Relationship.FAMILY);
 
         // Friend 2 and his 2 friends
         SocialNode friend2 = getFeatureNode(2, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend2.addRelationship(getFeatureNode(200, Feature.randomFeature()), Relationship.FAMILY);
-        friend2.addRelationship(getFeatureNode(201, Feature.randomFeature()), Relationship.FAMILY);
+        friend2.addRelationship(getFeatureNode(20, Feature.randomFeature()), Relationship.FAMILY);
+        friend2.addRelationship(getFeatureNode(21, Feature.randomFeature()), Relationship.FAMILY);
 
         // Friend 3 and his 2 friend
         SocialNode friend3 = getFeatureNode(3, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend3.addRelationship(getFeatureNode(300, Feature.randomFeature()), Relationship.FAMILY);
-        friend3.addRelationship(getFeatureNode(301, Feature.randomFeature()), Relationship.CO_WORKER);
+        friend3.addRelationship(getFeatureNode(30, Feature.randomFeature()), Relationship.FAMILY);
+        friend3.addRelationship(getFeatureNode(31, Feature.randomFeature()), Relationship.CO_WORKER);
 
         // Add friends to main
         main.addRelationship(friend1, Relationship.FAMILY.FRIEND);
@@ -150,17 +150,17 @@ public class DiscoverTests {
 
         // Friend 1 and his 1 friend
         SocialNode friend1 = getFeatureNode(1, Feature.SMART_LIGHTING, Feature.SMART_PARKING);
-        friend1.addRelationship(getFeatureNode(100, Feature.TANK_LEVEL_MONITOR), Relationship.FAMILY);
+        friend1.addRelationship(getFeatureNode(10, Feature.TANK_LEVEL_MONITOR), Relationship.FAMILY);
 
         // Friend 2 and his 2 friends
         SocialNode friend2 = getFeatureNode(2, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend2.addRelationship(getFeatureNode(200, Feature.WATER_LEAKAGE), Relationship.FAMILY);
-        friend2.addRelationship(getFeatureNode(201, Feature.SILOS_STOCK_MEASUREMENT), Relationship.FAMILY);
+        friend2.addRelationship(getFeatureNode(20, Feature.WATER_LEAKAGE), Relationship.FAMILY);
+        friend2.addRelationship(getFeatureNode(21, Feature.SILOS_STOCK_MEASUREMENT), Relationship.FAMILY);
 
         // Friend 3 and his 2 friend
         SocialNode friend3 = getFeatureNode(3, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend3.addRelationship(getFeatureNode(300, Feature.PORTABLE_WATER_MONITORING), Relationship.FAMILY);
-        friend3.addRelationship(getFeatureNode(301, Feature.SWIMMING_POOL_MONITOR), Relationship.CO_WORKER);
+        friend3.addRelationship(getFeatureNode(30, Feature.PORTABLE_WATER_MONITORING), Relationship.FAMILY);
+        friend3.addRelationship(getFeatureNode(31, Feature.SWIMMING_POOL_MONITOR), Relationship.CO_WORKER);
 
         // Add friends to main
         main.addRelationship(friend1, Relationship.FAMILY.FRIEND);
@@ -173,7 +173,7 @@ public class DiscoverTests {
 
         Assert.assertTrue(searchResult.isSuccess());
         Assert.assertEquals(8, searchResult.getBandwidth());
-        Assert.assertEquals(201, searchResult.getNode().getId());
+        Assert.assertEquals(21, searchResult.getNode().getId());
     }
 
     @Test()
@@ -188,19 +188,19 @@ public class DiscoverTests {
 
         // Friend 1 and his 1 friend
         SocialNode friend1 = getFeatureNode(1, Feature.SMART_LIGHTING, Feature.SMART_PARKING);
-        friend1.addRelationship(getFeatureNode(100, Feature.TANK_LEVEL_MONITOR), Relationship.FAMILY);
+        friend1.addRelationship(getFeatureNode(10, Feature.TANK_LEVEL_MONITOR), Relationship.FAMILY);
 
         // Friend 2 and his 4 friends
         SocialNode friend2 = getFeatureNode(2, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend2.addRelationship(getFeatureNode(200, Feature.WATER_LEAKAGE), Relationship.CO_WORKER);
-        friend2.addRelationship(getFeatureNode(201, Feature.SILOS_STOCK_MEASUREMENT), Relationship.CO_WORKER);
-        friend2.addRelationship(getFeatureNode(203, Feature.PHOTOVOLTAIC_MONITOR), Relationship.CO_WORKER);
-        friend2.addRelationship(getFeatureNode(204, Feature.TRAFFIC_CONGESTION), Relationship.CO_WORKER);
+        friend2.addRelationship(getFeatureNode(20, Feature.WATER_LEAKAGE), Relationship.CO_WORKER);
+        friend2.addRelationship(getFeatureNode(21, Feature.SILOS_STOCK_MEASUREMENT), Relationship.CO_WORKER);
+        friend2.addRelationship(getFeatureNode(23, Feature.PHOTOVOLTAIC_MONITOR), Relationship.CO_WORKER);
+        friend2.addRelationship(getFeatureNode(24, Feature.TRAFFIC_CONGESTION), Relationship.CO_WORKER);
 
         // Friend 3 and his 2 friend
         SocialNode friend3 = getFeatureNode(3, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend3.addRelationship(getFeatureNode(300, Feature.PORTABLE_WATER_MONITORING), Relationship.ACQUAINTANCE);
-        friend3.addRelationship(getFeatureNode(301, Feature.SWIMMING_POOL_MONITOR), Relationship.FRIEND);
+        friend3.addRelationship(getFeatureNode(30, Feature.PORTABLE_WATER_MONITORING), Relationship.ACQUAINTANCE);
+        friend3.addRelationship(getFeatureNode(31, Feature.SWIMMING_POOL_MONITOR), Relationship.FRIEND);
 
         // Add friends to main
         main.addRelationship(friend1, Relationship.FAMILY.FRIEND);
@@ -215,7 +215,7 @@ public class DiscoverTests {
         // Tests
         Assert.assertTrue(searchResult.isSuccess());
         Assert.assertEquals(7, searchResult.getBandwidth());
-        Assert.assertEquals(203, searchResult.getNode().getId());
+        Assert.assertEquals(23, searchResult.getNode().getId());
     }
 
     @Test()
@@ -227,19 +227,19 @@ public class DiscoverTests {
 
         // Friend 1 and his 1 friend
         SocialNode friend1 = getFeatureNode(1, Feature.SMART_LIGHTING, Feature.SMART_PARKING);
-        friend1.addRelationship(getFeatureNode(100, Feature.TANK_LEVEL_MONITOR), Relationship.FAMILY);
+        friend1.addRelationship(getFeatureNode(10, Feature.TANK_LEVEL_MONITOR), Relationship.FAMILY);
 
         // Friend 2 and his 4 friends
         SocialNode friend2 = getFeatureNode(2, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend2.addRelationship(getFeatureNode(200, Feature.WATER_LEAKAGE), Relationship.CO_WORKER);
-        friend2.addRelationship(getFeatureNode(201, Feature.SILOS_STOCK_MEASUREMENT), Relationship.CO_WORKER);
-        friend2.addRelationship(getFeatureNode(203, Feature.PHOTOVOLTAIC_MONITOR), Relationship.CO_WORKER);
-        friend2.addRelationship(getFeatureNode(204, Feature.TRAFFIC_CONGESTION), Relationship.CO_WORKER);
+        friend2.addRelationship(getFeatureNode(20, Feature.WATER_LEAKAGE), Relationship.CO_WORKER);
+        friend2.addRelationship(getFeatureNode(21, Feature.SILOS_STOCK_MEASUREMENT), Relationship.CO_WORKER);
+        friend2.addRelationship(getFeatureNode(23, Feature.PHOTOVOLTAIC_MONITOR), Relationship.CO_WORKER);
+        friend2.addRelationship(getFeatureNode(24, Feature.TRAFFIC_CONGESTION), Relationship.CO_WORKER);
 
         // Friend 3 and his 2 friend
         SocialNode friend3 = getFeatureNode(3, Feature.STRUCTURAL_HEALTH, Feature.TRAFFIC_CONGESTION);
-        friend3.addRelationship(getFeatureNode(300, Feature.PORTABLE_WATER_MONITORING), Relationship.ACQUAINTANCE);
-        friend3.addRelationship(getFeatureNode(301, Feature.SWIMMING_POOL_MONITOR), Relationship.FRIEND);
+        friend3.addRelationship(getFeatureNode(30, Feature.PORTABLE_WATER_MONITORING), Relationship.ACQUAINTANCE);
+        friend3.addRelationship(getFeatureNode(31, Feature.SWIMMING_POOL_MONITOR), Relationship.FRIEND);
 
         // Add friends to main
         main.addRelationship(friend1, Relationship.FAMILY.FRIEND);
@@ -255,6 +255,102 @@ public class DiscoverTests {
         Assert.assertFalse(searchResult.isSuccess());
         Assert.assertEquals(11, searchResult.getBandwidth());
         Assert.assertNull(searchResult.getNode());
+    }
+
+    @Test()
+    /* Should return true in a deep nested relationships */
+    public void discoverDeepDeepRelationshipTest2Pass() {
+        SocialNode main = getFeatureNode(0, Feature.NOISE);
+
+        // Friend 1 and his nested relationship
+        SocialNode friend1 = getFeatureNode(1, Feature.SMART_LIGHTING, Feature.SMART_PARKING);
+        SocialNode f1f1 = getFeatureNode(10, Feature.ELECTROMAGNETIC_FIELD_LEVELS);
+        SocialNode f1f2 = getFeatureNode(11, Feature.ELECTROMAGNETIC_FIELD_LEVELS);
+
+        friend1.addRelationship(f1f1, Relationship.ACQUAINTANCE);
+        f1f1.addRelationship(getFeatureNode(100, Feature.PHOTOVOLTAIC_MONITOR), Relationship.ACQUAINTANCE);
+        f1f1.addRelationship(getFeatureNode(101, Feature.PORTABLE_WATER_MONITORING), Relationship.ACQUAINTANCE);
+
+        friend1.addRelationship(f1f2, Relationship.ACQUAINTANCE);
+        f1f2.addRelationship(getFeatureNode(110, Feature.PHOTOVOLTAIC_MONITOR), Relationship.ACQUAINTANCE);
+        f1f2.addRelationship(getFeatureNode(111, Feature.PORTABLE_WATER_MONITORING), Relationship.FRIEND);
+
+        // Friend 2 and his nested relationship
+        SocialNode friend2 = getFeatureNode(2, Feature.SMART_LIGHTING, Feature.SMART_PARKING);
+        SocialNode f2f1 = getFeatureNode(20, Feature.ELECTROMAGNETIC_FIELD_LEVELS);
+        SocialNode f2f2 = getFeatureNode(21, Feature.TANK_LEVEL_MONITOR);
+
+        friend2.addRelationship(f2f1, Relationship.ACQUAINTANCE);
+        f2f1.addRelationship(getFeatureNode(200, Feature.PHOTOVOLTAIC_MONITOR), Relationship.ACQUAINTANCE);
+        f2f1.addRelationship(getFeatureNode(201, Feature.SMART_PARKING), Relationship.ACQUAINTANCE);
+
+        friend2.addRelationship(f2f2, Relationship.CO_WORKER);
+        f2f2.addRelationship(getFeatureNode(210, Feature.SMARTPHONE_DETECTION), Relationship.ACQUAINTANCE);
+        f2f2.addRelationship(getFeatureNode(211, Feature.PORTABLE_WATER_MONITORING), Relationship.FRIEND);
+
+
+        // Add friends to main
+        main.addRelationship(friend1, Relationship.FRIEND);
+        main.addRelationship(friend2, Relationship.FRIEND);
+
+        // The searched featured
+        Search searchResult = main.discover(Feature.SMARTPHONE_DETECTION);
+        System.out.println(searchResult);
+        System.out.println(searchResult.getNodeVisited());
+
+        // Tests
+        Assert.assertTrue(searchResult.isSuccess());
+        Assert.assertEquals(10, searchResult.getBandwidth());
+        Assert.assertEquals(210, searchResult.getNode().getId());
+    }
+
+    @Test()
+    /* Should return true/right node in a deep nested relationships
+    *  and the feature appear at depth = 1 and 3;
+    */
+    public void discoverDeepDeepRelationshipTest3Pass() {
+        SocialNode main = getFeatureNode(0, Feature.NOISE);
+
+        // Friend 1 and his nested relationship
+        SocialNode friend1 = getFeatureNode(1, Feature.SMART_LIGHTING, Feature.SMART_PARKING);
+        SocialNode f1f1 = getFeatureNode(10, Feature.ELECTROMAGNETIC_FIELD_LEVELS);
+        SocialNode f1f2 = getFeatureNode(11, Feature.ELECTROMAGNETIC_FIELD_LEVELS);
+
+        friend1.addRelationship(f1f1, Relationship.ACQUAINTANCE);
+        f1f1.addRelationship(getFeatureNode(100, Feature.PORTABLE_WATER_MONITORING), Relationship.ACQUAINTANCE);
+        f1f1.addRelationship(getFeatureNode(101, Feature.PORTABLE_WATER_MONITORING), Relationship.ACQUAINTANCE);
+
+        friend1.addRelationship(f1f2, Relationship.ACQUAINTANCE);
+        f1f2.addRelationship(getFeatureNode(110, Feature.PHOTOVOLTAIC_MONITOR), Relationship.ACQUAINTANCE);
+        f1f2.addRelationship(getFeatureNode(111, Feature.PORTABLE_WATER_MONITORING), Relationship.FRIEND);
+
+        // Friend 2 and his nested relationship
+        SocialNode friend2 = getFeatureNode(2, Feature.SMART_LIGHTING, Feature.SMART_PARKING);
+        SocialNode f2f1 = getFeatureNode(20, Feature.TRAFFIC_CONGESTION);
+        SocialNode f2f2 = getFeatureNode(21, Feature.TANK_LEVEL_MONITOR);
+
+        friend2.addRelationship(f2f1, Relationship.ACQUAINTANCE);
+        f2f1.addRelationship(getFeatureNode(200, Feature.TRAFFIC_CONGESTION), Relationship.ACQUAINTANCE);
+        f2f1.addRelationship(getFeatureNode(201, Feature.SMART_PARKING), Relationship.ACQUAINTANCE);
+
+        friend2.addRelationship(f2f2, Relationship.CO_WORKER);
+        f2f2.addRelationship(getFeatureNode(210, Feature.SMARTPHONE_DETECTION), Relationship.ACQUAINTANCE);
+        f2f2.addRelationship(getFeatureNode(211, Feature.PORTABLE_WATER_MONITORING), Relationship.FRIEND);
+
+
+        // Add friends to main
+        main.addRelationship(friend1, Relationship.FRIEND);
+        main.addRelationship(friend2, Relationship.FRIEND);
+
+        // The searched featured
+        Search searchResult = main.discover(Feature.TRAFFIC_CONGESTION);
+        System.out.println(searchResult);
+        System.out.println(searchResult.getNodeVisited());
+
+        // Tests
+        Assert.assertTrue(searchResult.isSuccess());
+        Assert.assertEquals(4, searchResult.getBandwidth());
+        Assert.assertEquals(20, searchResult.getNode().getId());
     }
     /* ******************************************* */
     /* *************** SET UP DATA *************** */
